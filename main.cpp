45f69e9b28e2c005d2dfebf8fdd88b4e26a44536
redthing1 <redthing1@alt.icu>
2023-03-24 14:28:59 -0700

    print ready signal

--- main.cpp
+++ main.cpp
@@ -312,7 +312,9 @@ int main(int argc, char ** argv) {
                " - Press Ctrl+C to interject at any time.\n"
 #endif
                " - Press Return to return control to LLaMa.\n"
-               " - If you want to submit another line, end your input in '\\'.\n\n");
+               " - If you want to submit another line, end your input in '\\'.\n"
+               "====\n"
+               "\n");
         is_interacting = params.interactive_start || params.instruct;
     }
 
